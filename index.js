bb31e131d2fe06f100f1623a3694d041e9beb12a
pallabofficial <officialpallabmondal000@gmail.com>
2024-01-31 13:25:10 +0530

    added animations: used gsap methods to animate the whole landing page

---
+++ index.js
@@ -0,0 +1,50 @@
+console.log("hello");
+let tl = gsap.timeline();
+
+tl
+
+    .from("#navbar", {
+        opacity:0,
+        y:-50,
+        duration:1,
+        ease:Power1.easeInOut,
+        stagger:0,
+    });
+   gsap.from("#banner-img-1", {
+        opacity:0,
+        x:-500,
+        duration:1,
+        ease:Power1.easeInOut,
+        delay:1.1,
+    });
+    gsap.from("#banner-img-2", {
+        opacity:0,
+        x:500,
+        duration:1,
+        ease:Power1.easeInOut,
+        delay:1.1,
+    });
+    gsap.from("#banner-img-3", {
+        opacity:0,
+        x:-500,
+        duration:1,
+        ease:Power1.easeInOut,
+        delay:1.1,
+    });
+    gsap.from("#banner-img-4", {
+        opacity:0,
+        x:500,
+        duration:1,
+        ease:Power1.easeInOut,
+        delay:1.1,
+    });
+    gsap.from("#banner-title, #banner-btn, #banner-drop-down",{
+        delay:2.1,
+        opacity:0,
+        duration:1,
+        ease:Power1.easeInOut,
+    });
+    
+    
+   
+
